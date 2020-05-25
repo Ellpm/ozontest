cab57737a57045c0100dbaa1b422ccb77d97406b
Ellpm <ellpm@ya.ru>
2020-05-25 19:26:56 +0300

    fix bags, stable version with asynx method

--- src/components/NumberBtn/NumberBtn.tsx
+++ src/components/NumberBtn/NumberBtn.tsx
@@ -1,24 +1,28 @@
 import { Component, Prop, Vue } from "vue-property-decorator";
 import { VueComponent } from "../../shims-vue";
-import {useStore} from 'vuex-simple'
-import {MyStore} from '../../store/store'
+import { useStore } from "vuex-simple";
+import { MyStore } from "../../store/store";
 
 import styles from "./NumberBtn.css?module";
 
 interface Props {
-  value: number|string;
+  value: number | string;
 }
 
 @Component
 export default class NumberBtn extends VueComponent<Props> {
-  
-  public store: MyStore = useStore(this.$store)
-
-  
- 
+  public store: MyStore = useStore(this.$store);
 
   @Prop() private value!: number;
   render() {
     return (
-    <div onClick={()=>{this.store.calc.addSymbol(this.value)}}>{this.value}</div>
-    )}}
+      <div
+        onClick={() => {
+          this.store.calc.addSymbol(this.value);
+        }}
+      >
+        {this.value}
+      </div>
+    );
+  }
+}
--- src/store/modules/calc.ts
+++ src/store/modules/calc.ts
@@ -1,33 +1,48 @@
-import { Getter, Mutation, State } from "vuex-simple";
+import { Mutation, State } from "vuex-simple";
 
 export class Calc {
-         @State() public buffer: string;
-         @State() public result: number;
+  @State() public buffer: string;
+  @State() public result: number;
+  @State() public operatorToggle: boolean;
 
-         constructor(buf: string = "", res: number = 0) {
-           this.buffer = buf;
-           this.result = res;
-         }
+  constructor(buf: string = "", res: number = 0, opTgl: boolean = false) {
+    this.buffer = buf;
+    this.result = res;
+    this.operatorToggle = opTgl;
+  }
 
-         @Mutation()
-         public addSymbol(symbol: number | string) {
-           this.buffer = this.buffer + symbol.toString();
-         }
-         @Mutation()
-         public clear(): void {
-           this.buffer = "";
-           this.result = 0;
-         }
-         @Mutation()
-         public equal(): void {
-           if (this.buffer.slice(-1) !== "+" && "-") {
-             this.result = eval(this.buffer);
-           }
-         }
-
-         public async asyncIncrement() {
-           await new Promise((r) => setTimeout(r, 200));
-           // call mutation function like you would any other function
-           // this.increment();
-         }
-       }
+  @Mutation()
+  public addSymbol(symbol: number | string) {    
+    if (
+      (this.operatorToggle && symbol === "+") ||
+      (this.operatorToggle && symbol === "-")
+    ) {
+      this.buffer =
+        this.buffer.slice(0, this.buffer.length - 1) + symbol.toString();
+      this.operatorToggle = true;
+    } else {
+      this.buffer = this.buffer + symbol.toString();
+      if (symbol === "+" || symbol === "-") {
+        this.operatorToggle = true;
+      } else {
+        this.operatorToggle = false;
+      }
+    }
+  }
+  @Mutation()
+  public clear(): void {
+    this.buffer = "";
+    this.result = 0;
+  }
+  @Mutation()
+  public async equal() {
+    if (!this.operatorToggle) {
+      await new Promise((res) =>
+        setTimeout(() => {
+          this.result = eval(this.buffer);
+          this.buffer = '';
+        }, 2000)
+      );
+    }
+  }
+}
