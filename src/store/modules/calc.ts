ae86f4d4b6cc3ebcca8008132d0b81742daf1af6
Ellpm <ellpm@ya.ru>
2020-05-26 11:22:20 +0300

    fix async bag

--- src/store/modules/calc.ts
+++ src/store/modules/calc.ts
@@ -4,45 +4,61 @@ export class Calc {
   @State() public buffer: string;
   @State() public result: string;
   @State() public operatorToggle: boolean;
+  @State() public isFetching: boolean;
 
-  constructor(buf: string = "", res: string = "0", opTgl: boolean = false) {
+  constructor(
+    buf: string = "",
+    res: string = "0",
+    opTgl: boolean = false,
+    isF: boolean = false
+  ) {
     this.buffer = buf;
     this.result = res;
     this.operatorToggle = opTgl;
+    this.isFetching = isF;
   }
 
   @Mutation()
-  public addSymbol(symbol: number | string) {    
-    if (
-      (this.operatorToggle && symbol === "+") ||
-      (this.operatorToggle && symbol === "-")
-    ) {
-      this.buffer =
-        this.buffer.slice(0, this.buffer.length - 1) + symbol.toString();
-      this.operatorToggle = true;
-    } else {
-      this.buffer = this.buffer + symbol.toString();
-      if (symbol === "+" || symbol === "-") {
+  public addSymbol(symbol: number | string) {
+    if (!this.isFetching) {
+      if (
+        (this.operatorToggle && symbol === "+") ||
+        (this.operatorToggle && symbol === "-")
+      ) {
+        this.buffer =
+          this.buffer.slice(0, this.buffer.length - 1) + symbol.toString();
         this.operatorToggle = true;
       } else {
-        this.operatorToggle = false;
+        this.buffer = this.buffer + symbol.toString();
+        if (symbol === "+" || symbol === "-") {
+          this.operatorToggle = true;
+        } else {
+          this.operatorToggle = false;
+        }
       }
     }
   }
+
   @Mutation()
   public clear(): void {
-    this.buffer = "";
-    this.result = "0";
+    if (!this.isFetching) {
+      this.buffer = "";
+      this.result = "0";
+    }
   }
   @Mutation()
   public async equal() {
-    if (!this.operatorToggle) {
-      await new Promise((res) =>
-        setTimeout(() => {
-          this.result = `= ${eval(this.buffer)}`;
-          this.buffer = '';
-        }, 2000)
-      );
+    if (!this.isFetching) {
+      this.isFetching = true;
+      if (!this.operatorToggle) {
+        await new Promise((res) =>
+          setTimeout(() => {
+            this.result = `= ${eval(this.buffer)}`;
+            this.buffer = "";
+            this.isFetching = false;
+          }, 2000)
+        );
+      }
     }
   }
 }
